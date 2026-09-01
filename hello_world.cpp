3bd808343f86456e838031cec24729d53b4f7b8f
KennyC555 <cjr070420@gmail.com>
2026-08-31 23:48:19 -0700

    Create hello_world.cpp
    finish hello world cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "My name is Jiarong cai." << endl;
+    cout << "Today is Monday";
+
+    system("pause>0");
+}
