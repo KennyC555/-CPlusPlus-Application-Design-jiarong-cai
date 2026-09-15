8ab65fa1d5967dc1839006b5ac97b3ba812a4eba
KennyC555 <cjr070420@gmail.com>
2026-09-14 23:14:27 -0700

    Complete Module 3 variables assignment

---
+++ Module03-Datasets-Arrays-Pointers/module3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string userName = "Jiarong";
+    string appName = "Student Helper";
+    double version = 1.0;
+
+    cout << "============================" << endl;
+    cout << " Welcome To My Application" << endl;
+    cout << "============================" << endl;
+
+    cout << "User: " << userName << endl;
+    cout << "Application: " << appName << endl;
+    cout << "Version: " << version << endl;
+
+    return 0;
+}
